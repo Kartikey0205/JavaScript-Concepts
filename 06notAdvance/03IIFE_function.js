20b948ed551956e2be67627475f837de69497e07
Kartikey Dubey <66429038+Kartikey0205@users.noreply.github.com>
2021-05-16 13:10:41 +0530

    Add files via upload

---
+++ 06notAdvance/03IIFE_function.js
@@ -0,0 +1,13 @@
+//Self Executing Annoynomous Function --> self executing means which is called by own and annoynomous means without any name
+/*
+var sayHello =  function(){};
+function name(params) {};
+var sayHello = () =>{};
+var sayHello = () =>();
+*/
+
+//Self Executing Annoynomous Function 
+
+(function(){
+    console.log("Hello i'm Self Executing Annoynomous Function because nobody is calling me and i don't have name too");
+})();
